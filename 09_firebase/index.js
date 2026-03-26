cea527381075950b6964eafd4d825724cf6e6cb6
Milasso69 <milassti@gmail.com>
2026-03-26 09:40:25 +0100

    GAY

--- 09_firebase/index.js
+++ 09_firebase/index.js
@@ -1,6 +1,16 @@
 
 
-//P5 setup() bliver kaldt EN gang før siden vises 
-function setup(){
-}
+//Lav en ref tul din collection
+var testRef = db.collection('test')
+console.log('Oprettet reference til test')
 
+//P5 setup() bliver kaldt EN gang før siden vises 
+function setup() {
+    testRef.onSnapshot(snap => {
+        console.log('Modtog snap', snap.size)
+        snap.forEach( doc => {
+            var d = doc.data()
+            console.log(d)
+        })
+    })
+}
